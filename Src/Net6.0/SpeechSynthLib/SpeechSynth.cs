cfbda9098d83e5804488d24420351251d250d04d
AlexPi <alex.pigida@outlook.com>
2020-12-13 22:27:00 -0500

    Update SpeechSynth.cs
    added interchangable neural voices + expression styles.

--- Src/Net6.0/SpeechSynthLib/SpeechSynth.cs
+++ Src/Net6.0/SpeechSynthLib/SpeechSynth.cs
@@ -35,7 +35,8 @@ namespace SpeechSynthLib
 
     public SpeechSynthesizer SynthReal => _synth ??= new SpeechSynthesizer(SpeechConfig.FromSubscription(_asc.Key, _asc.Rgn));
 
-    public async Task SpeakAsync(string msg, string k="")
+    int _s = 0, _v = 0;
+    public async Task SpeakAsync(string msg, string k = "")
     {
       try
       {
@@ -45,43 +46,38 @@ namespace SpeechSynthLib
           return;
         }
 
-        /*
-<speak version="1.0" xmlns="https://www.w3.org/2001/10/synthesis" xml:lang="en-US">
-  <voice name="en-GB-George-Apollo">
-    <prosody rate="0.9">
-      When you're on the motorway,<break time="200ms"/> it's a good idea to use a sat-nav.
-    </prosody>
-  </voice>
-</speak>
-<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">
-  <voice name="en-US-AriaNeural">
-    <mstts:express-as style="cheerful">
-      This is awesome!
-    </mstts:express-as>
-  </voice>
-</speak>
-        */
+        var styles = new[] { // https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/speech-synthesis-markup?tabs=csharp#adjust-speaking-styles
+           "newscast-formal"   ,  // Expresses a formal, confident and authoritative tone for news delivery
+           "newscast-casual"   ,  // Expresses a versatile and casual tone for general news delivery
+           "customerservice"   ,  // Expresses a friendly and helpful tone for customer support
+           "chat"              ,  // Expresses a casual and relaxed tone
+           "cheerful"          ,  // Expresses a positive and happy tone
+           "empathetic" };        // Expresses a sense of caring and understanding
+
+        var voices = new[] { // https://docs.microsoft.com/en-us/azure/cognitive-services/containers/container-image-tags?tabs=current
+          "en-gb-george-apollo",  // Container image with the en-GB locale and en-GB-George-Apollo voice.	
+          "en-gb-hazelrus",       // Container image with the en-GB locale and en-GB-HazelRUS voice. 
+          "en-gb-susan-apollo"};	// Container image with the en-GB locale and en-GB-Susan-Apollo voice.	
+
         using var result = await SynthReal.SpeakSsmlAsync(
-          k == "Faf" ?
+k == "Faf" ? // std voice from https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#standard-voices
 $@"
 <speak version=""1.0"" xmlns=""https://www.w3.org/2001/10/synthesis"" xml:lang=""en-US"">
-  <voice name=""en-GB-George-Apollo"">
-    <prosody rate=""1.25"">
-      {msg}, <break time=""100ms""/> {k}.
+  <voice name=""{voices[_v % voices.Length]}"">
+    <prosody rate=""1.4"">
+      {msg}, <break time=""100ms""/> {voices[_v++ % voices.Length]}.
     </prosody>
   </voice>
-</speak>
-" : 
+</speak>"
+:                   // neural voices from https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#neural-voices
 $@"
 <speak version=""1.0"" xmlns=""http://www.w3.org/2001/10/synthesis"" xmlns:mstts=""https://www.w3.org/2001/mstts"" xml:lang=""en-US"">
   <voice name=""en-US-AriaNeural"">
-    <mstts:express-as style=""cheerful"">
-      {msg}, <break time=""100ms""/> {k}.
+    <mstts:express-as style=""{styles[_s % styles.Length]}"">
+      {msg}, <break time=""100ms""/> {styles[_s++ % styles.Length]}.
     </mstts:express-as>
   </voice>
-</speak>
-  ");
-        //using var result = await SynthReal.SpeakTextAsync(msg);
+</speak>");
 
         if (result.Reason == ResultReason.SynthesizingAudioCompleted)
         {
