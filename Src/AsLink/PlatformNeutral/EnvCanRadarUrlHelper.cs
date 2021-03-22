353987f16c62d908608a1920836d3a44563832c7
AlexPi <alex.pigida@outlook.com>
2021-03-21 21:39:18 -0400

    Update EnvCanRadarUrlHelper.cs

--- Src/AsLink/PlatformNeutral/EnvCanRadarUrlHelper.cs
+++ Src/AsLink/PlatformNeutral/EnvCanRadarUrlHelper.cs
@@ -1,26 +1,20 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace AsLink
 {
   public class EnvCanRadarUrlHelper
   {
-    public static DateTime RoundBy10min(DateTime dt) => dt.AddTicks(-dt.Ticks % (10 * TimeSpan.TicksPerMinute));
-    public static string GetRadarUrl(DateTime d, string rsRainOrSnow /*= "RAIN"*/, string station /*= "WKR"*/, bool isFallbackCAPPI, bool? isDark /*= null*/, bool isFallbackCOMP = false)
+    public static string GetRadarUrl(DateTime d, string rsRainOrSnow /*= "RAIN"*/, string station /*= "WKR"*/, bool isFallbackCAPPI, bool isFallbackCOMP = false)
     {
-      isDark = isDark ?? IsDark; // isDark ??= IsDark; in UWP gives Error CS8370  Feature 'coalescing assignment' is not available in C# 7.3. Please use language version 8.0 or greater.	
-
-      ///todo: on error try this _COMP_
       var cmp = isFallbackCOMP ? "_COMP" : "";
       var cap = isFallbackCAPPI ? "CAPPI" : "PRECIP";
-      var drk = isDark == true ? "" : "detailed/";
+      var drk = IsDark ? "" : "detailed/";
 
-      var url = $"https://weather.gc.ca/data/radar/{drk}temp_image//{station}/{station}{cmp}_{cap}_{rsRainOrSnow}_{d.Year}_{d.Month:0#}_{d.Day:0#}_{d.Hour:0#}_{d.Minute:0#}.GIF";
-
-      return url;
+      return $"https://weather.gc.ca/data/radar/{drk}temp_image//{station}/{station}{cmp}_{cap}_{rsRainOrSnow}_{d.Year}_{d.Month:0#}_{d.Day:0#}_{d.Hour:0#}_{d.Minute:0#}.GIF";
     }
 
-    public static bool IsDark { get => DateTime.Now.Hour < 8 || 20 <= DateTime.Now.Hour; }
+    public static DateTime RoundBy10min(DateTime dt) => dt.AddTicks(-dt.Ticks % (10 * TimeSpan.TicksPerMinute));
+    public static bool IsDark => DateTime.Now.Hour < 8 || 20 <= DateTime.Now.Hour;
   }
 }
 /*
