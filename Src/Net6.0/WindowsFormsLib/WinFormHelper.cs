3fd1bd03bf0a43ed6eb59105d47e4f851578b65f
AlexPi <alex.pigida@outlook.com>
2021-05-25 09:11:43 -0400

    moved to here

---
+++ Src/Net6.0/WindowsFormsLib/WinFormHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsLib
+{
+  public class WinFormHelper
+  {
+    public static Screen[] GetAllScreens() => Screen.AllScreens;
+    public static Screen PrimaryScreen => Screen.PrimaryScreen;
+  }
+
+  static class Program {[STAThread] static void Main___() { } }
+}
