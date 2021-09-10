1294763bfc184359199490e6f912b8a514b12e99
AlexPi <alex.pigida@outlook.com>
2021-09-09 21:42:27 -0400

    Update EvLogHelperBase.cs

--- Src/AsLink/PlatformNeutral/EvLogHelperBase.cs
+++ Src/AsLink/PlatformNeutral/EvLogHelperBase.cs
@@ -5,16 +5,10 @@ using System.Security.Principal;
 
 namespace AsLink
 {
-  public static partial class EvLogHelper //this is the latest copy as of Sep 9, 2021.
+  public static partial class EvLogHelper //this is the latest copy as of Sep 9, 2021. 
   {
     const string _app = "Application", _sec = "Security", _sys = "System", _aavSource = "AavSource", _aavLogName = "AavNewLog";
-    public static string CheckCreateLogChannel(string src = _aavSource, string log = _aavLogName) // which one is the latest
-    {
-      if (!IsAdministrator())
-        return ("Restart as Admin!!!  (...to query/create event log)");
-      else
-        return safeCreateEventSource(src, log);
-    }
+    public static string CheckCreateLogChannel(string src = _aavSource, string log = _aavLogName) => !IsAdministrator() ? "Restart as Admin!!!  (...to query/create event log)" : safeCreateEventSource(src, log);
     public static bool IsAdministrator()
     {
       var identity = WindowsIdentity.GetCurrent();
@@ -26,16 +20,8 @@ namespace AsLink
 
       return false;
     }
-    public static void LogScrSvrBgn(int App_Ssto_Gp, string msg = "ScrSvr - Up. ScreenSaveTimeOut + GracePeriod ") { try { EventLog.WriteEntry(_aavSource, $"{msg}(sec) ={App_Ssto_Gp}", EventLogEntryType.Information, 7101); } catch (Exception ex) { ex.Log(); } }// AavSource finds the log named AavNewLog    
-    public static void LogScrSvrEnd(DateTime actualIdleIeStartApp, int ssTimeoutSec, string msg)
-    {
-      try
-      {
-        var dt = DateTime.Now - actualIdleIeStartApp;
-        EventLog.WriteEntry(_aavSource, $"Idle since {actualIdleIeStartApp:HH:mm:ss} (=App.Start) for {dt:h\\:mm\\:ss}    {msg}", EventLogEntryType.Information, 7102);
-      }
-      catch (Exception ex) { ex.Log(); }
-    }// AavSource finds the log named AavNewLog
+    public static void LogScrSvrBgn(int App_Ssto_Gp, string msg = "ScrSvr - Up. ScreenSaveTimeOut + GracePeriod 1 min") { try { EventLog.WriteEntry(_aavSource, $"{msg}(sec) ={App_Ssto_Gp}", EventLogEntryType.Information, 7101); } catch (Exception ex) { ex.Log(); } }// AavSource finds the log named AavNewLog    
+    public static void LogScrSvrEnd(DateTime actualIdleIeStartApp, string msg) { try { EventLog.WriteEntry(_aavSource, $"Idle since {actualIdleIeStartApp:HH:mm:ss} (=App.Start) for {DateTime.Now - actualIdleIeStartApp:h\\:mm\\:ss}    {msg}", EventLogEntryType.Information, 7102); } catch (Exception ex) { ex.Log(); } }// AavSource finds the log named AavNewLog
 
     static string safeCreateEventSource(string src, string log)
     {
