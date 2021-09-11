4c93eed1fbe465c0b9c31fb708d1b957d0d66372
AlexPi <alex.pigida@outlook.com>
2021-09-11 09:06:24 -0400

    Update EvLogHelperBase.cs

--- Src/AsLink/PlatformNeutral/EvLogHelperBase.cs
+++ Src/AsLink/PlatformNeutral/EvLogHelperBase.cs
@@ -9,11 +9,12 @@ namespace AsLink
   {
     const string _app = "Application", _sec = "Security", _sys = "System", _aavSource = "AavSource", _aavLogName = "AavNewLog";
 
-    public static string CheckCreateLogChannel(string src = _aavSource, string log = _aavLogName) => !IsAdministrator() ? "Restart as Admin!!!  (...to query/create event log)" : safeCreateEventSource(src, log);
     public static bool IsAdministrator() => WindowsIdentity.GetCurrent() != null && new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+    
     public static void LogScrSvrBgn(int App_Ssto_Gp, string msg = "ScrSvr - Up. ScreenSaveTimeOut + GracePeriod 1 min") { try { EventLog.WriteEntry(_aavSource, $"{msg}(sec) ={App_Ssto_Gp}", EventLogEntryType.Information, 7101); } catch (Exception ex) { ex.Log(); } }// AavSource finds the log named AavNewLog    
     public static void LogScrSvrEnd(DateTime actualIdleIeStartApp, string msg = "------------------------------------") { try { EventLog.WriteEntry(_aavSource, $"Idle since {actualIdleIeStartApp:HH:mm:ss} (=App.Start) for {DateTime.Now - actualIdleIeStartApp:h\\:mm\\:ss}    {msg}", EventLogEntryType.Information, 7102); } catch (Exception ex) { ex.Log(); } }// AavSource finds the log named AavNewLog
 
+    public static string CheckCreateLogChannel(string src = _aavSource, string log = _aavLogName) => !IsAdministrator() ? "Restart as Admin!!!  (...to query/create event log)" : safeCreateEventSource(src, log);
     static string safeCreateEventSource(string src, string log)
     {
       if (EventLog.SourceExists(src))
