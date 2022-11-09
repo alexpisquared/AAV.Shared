e790b69bf6c369f5ca23021d4c6f5bd6b92da836
AlexPi <alex.pigida@outlook.com>
2022-11-08 21:12:31 -0500

    ver 7.0

--- Src/Net.CI/WpfUserControlLib/Helpers/ConfigHelper.cs
+++ Src/Net.CI/WpfUserControlLib/Helpers/ConfigHelper.cs
@@ -65,7 +65,7 @@ public class ConfigHelper //todo:  appsettings as user prefs: https://makolyte.c
     cfg[CfgName.ServerLst] = StandardLib.Base.Consts.SqlServerCSV;
     cfg[CfgName.DtBsNmLst] = StandardLib.Base.Consts.DtBsNameCSV;
     cfg[CfgName.LogFolder] = VersionHelper.IsDbg ? @"C:\Temp\Logs\..log" : @"Z:\Dev\_Redis_MTDEV\BMS\IncomePaymentManagement\Logs\..log";
-    cfg[CfgName.SqlVerIpm] = VersionHelper.IsDbg ? "Server={0};Database={1};Trusted_Connection=True;Connection Timeout=15;" : "Server={0};Database={1};Trusted_Connection=True;Connection Timeout=52;"; // On dbg 5 sec was fine .. let's see if 1 sec is going to cause YY's issues.
+    cfg[CfgName.SqlVerIpm] = VersionHelper.IsDbg ? "Server={0};Database={1};Trusted_Connection=True;Encrypt=False;Connection Timeout=15;" : "Server={0};Database={1};Trusted_Connection=True;Encrypt=False;Connection Timeout=52;"; // On dbg 5 sec was fine .. let's see if 1 sec is going to cause YY's issues.
     cfg[CfgName.SqlVerIp_] = "Server={0};Database={1};persist security info=True;user id={2};password={3};MultipleActiveResultSets=True;App=EntityFramework;Connection Timeout=57";
 
 #if !true
@@ -86,7 +86,7 @@ public class ConfigHelper //todo:  appsettings as user prefs: https://makolyte.c
       ""LogFolder"":            ""Z:\\Dev\\_Redis_MTDEV\\BMS\\IncomePaymentManagement\\Logs\\..log"",
       ""ServerLst"":            ""mtDEVsqldb,1625 mtUATsqldb mtPRDsqldb .\\sqlexpress"",
       ""DtBsNmLst"":            ""QStatsDbg QStatsRls"",
-      ""SqlConStrFormat"":      ""Server={{0}};Database={{1}};Trusted_Connection=True;Connection Timeout=41;"",
+      ""SqlConStrFormat"":      ""Server={{0}};Database={{1}};Trusted_Connection=True;Encrypt=False;Connection Timeout=41;"",
       ""SqlConStrForma_"":      ""Server={{0}};Database={{1}};persist security info=True;user id={{2}};password={{3}};MultipleActiveResultSets=True;App=EntityFramework;Connection Timeout=47"",
       ""SubDetails"": {{
         ""KeyVaultURL"":        ""<moved to a safe place>"",
