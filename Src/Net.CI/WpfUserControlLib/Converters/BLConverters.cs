47021d8fb316f60c0515b0fe7724ac5167c7e510
AlexPi <alex.pigida@outlook.com>
2022-11-21 11:28:42 -0500

    phone list to page1

--- Src/Net.CI/WpfUserControlLib/Converters/BLConverters.cs
+++ Src/Net.CI/WpfUserControlLib/Converters/BLConverters.cs
@@ -25,3 +25,29 @@ public class DropListConverter : MarkupExtension, IValueConverter
   public override object ProvideValue(IServiceProvider serviceProvider) => this;
   public DropListConverter() { }
 }
+
+public class PhoneConverter : MarkupExtension, IValueConverter
+{
+  public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+  {
+    try
+    {
+      if (!(value is string str && !string.IsNullOrEmpty(str)))
+        return Brushes.Gray;
+
+      return
+        str.Equals("A", StringComparison.OrdinalIgnoreCase) ? Brushes.Green :
+        str.Equals("U", StringComparison.OrdinalIgnoreCase) ? Brushes.Blue :
+        str.Equals("D", StringComparison.OrdinalIgnoreCase) ? Brushes.Red :
+        str.StartsWith("#") ? new SolidColorBrush((Color)ColorConverter.ConvertFromString(str)) : // ColorRGB
+        Brushes.Brown;
+    }
+    catch
+    {
+      return Brushes.Red;
+    }
+  }
+  public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => "";
+  public override object ProvideValue(IServiceProvider serviceProvider) => this;
+  public PhoneConverter() { }
+}
