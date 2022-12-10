0c934f3f19f0be121585a22108a88b38b2d5c8d4
AlexPi <alex.pigida@outlook.com>
2022-12-10 13:20:45 -0500

    GptPoc

--- Src/Net6.0/AAV.Sys/Helpers/SqlConStrHelper.cs
+++ Src/Net6.0/AAV.Sys/Helpers/SqlConStrHelper.cs
@@ -3,33 +3,31 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
-namespace AAV.Sys.Helpers
+namespace AAV.Sys.Helpers;
+
+public static class SqlConStrHelper
 {
-  public static class SqlConStrHelper
-  {
-    public enum DbLocation { Azure, Local, DbIns };
+  public enum DbLocation { Azure, Local, DbIns };
 
-    public static string? DbNameFind(string conStr) => conStr?.Split(';').ToList().FirstOrDefault(r => r.Split('=')[0].Equals("initial catalog"
+  public static string? DbNameFind(string conStr) => conStr?.Split(';').ToList().FirstOrDefault(r => r.Split('=')[0].Equals("initial catalog"
+    , StringComparison.OrdinalIgnoreCase
+    ) || r.Split('=')[0].Equals("AttachDbFilename"
       , StringComparison.OrdinalIgnoreCase
-      ) || r.Split('=')[0].Equals("AttachDbFilename"
-        , StringComparison.OrdinalIgnoreCase
-        ))?.Split('=')[1];
+      ))?.Split('=')[1];
 
-    public static string ConStr(string dbName, string dbgRLs, DbLocation dbLocation, string username, string password)
+  public static string ConStr(string dbName, string dbgRLs, DbLocation dbLocation, string username, string password)
+  {
+    var conStr = "°°°";
+    try
     {
       switch (dbLocation)
       {
-        case DbLocation.Azure:
-          return $"data source=sqs.database.windows.net;initial catalog={dbName}{dbgRLs};persist security info=True;user id={username};password=\"{password}\";MultipleActiveResultSets=True;App=EntityFramework";
-        case DbLocation.Local:
-          var dbpathfilename = OneDrive.Folder($@"Public\AppData\TimeTrack\{dbName}{dbgRLs}.mdf");
-          Debug.Assert(File.Exists(dbpathfilename));
-          return $@"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename={dbpathfilename};Integrated Security=True;Connect Timeout=10;";
-        case DbLocation.DbIns:
-          return $@"data source=.\sqlexpress;initial catalog={dbName}{dbgRLs}; integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
-        default:
-          throw new ArgumentOutOfRangeException(dbLocation.GetType().Name);
+        case DbLocation.DbIns: return conStr = $@"data source=.\sqlexpress;initial catalog={dbName}{dbgRLs}; integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+        case DbLocation.Azure: return conStr = $"data source=sqs.database.windows.net;initial catalog={dbName}{dbgRLs};persist security info=True;user id={username};password=\"{password}\";MultipleActiveResultSets=True;App=EntityFramework";
+        case DbLocation.Local: var dbpathfilename = OneDrive.Folder($@"Public\AppData\TimeTrack\{dbName}{dbgRLs}.mdf"); Debug.Assert(File.Exists(dbpathfilename)); return conStr = $@"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename={dbpathfilename};Integrated Security=True;Connect Timeout=10;";
+        default: throw new ArgumentOutOfRangeException(dbLocation.GetType().Name);
       }
     }
+    finally { Trace.WriteLine($">> {conStr}"); }
   }
 }
