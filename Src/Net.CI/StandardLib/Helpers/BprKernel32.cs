531ecd7ff153fcf43ee14c6176e432958c6bb807
AlexPi <alex.pigida@outlook.com>
2022-12-14 09:24:39 -0500

    -CI

--- Src/Net.CI/StandardLib/Helpers/BprKernel32.cs
+++ Src/Net.CI/StandardLib/Helpers/BprKernel32.cs
@@ -1,28 +1,30 @@
-﻿using System.Threading.Tasks;
+﻿namespace StandardLib.Helpers;
 
-namespace StandardLib.Helpers
+//[System.Obsolete] - only for exceptional situations when proper beeper is not available.
+public class BprKernel32
 {
-  //[System.Obsolete] - only for exceptional situations when proper beeper is not available.
-  internal class BprKernel32
+  public static void Start() { Beep(_7kHz, _msA); Beep(_8kHz, _msB); }
+  public static void Finish() { Beep(_8kHz, _msB); Beep(_7kHz, _msA); }
+  public static void Click() => NativeMethods.BeepIf(600, 120);
+  public static void Tick() => NativeMethods.BeepIf(400, 120);
+  public static void Warn() => NativeMethods.BeepIf(8000, 120);
+  public static void Error() // CI-RDP-adjusted.
   {
-    public static void Start() { Beep(_7kHz, _msA); Beep(_8kHz, _msB); }
-    public static void Finish() { Beep(_8kHz, _msB); Beep(_7kHz, _msA); }
-    public static void Tick() => NativeMethods.BeepIf(8000, 120);
-    public static void Error() // CI-RDP-adjusted.
-    {
-      NativeMethods.BeepIf(_6kHz, 200);
-      NativeMethods.BeepIf(_6kHz, 200);
-      NativeMethods.BeepIf(_6kHz, 200);
-      NativeMethods.BeepIf(_5kHz, 800);
-    }
-
-    public static void ErrorFaF() => Task.Run(() => Error());
-    public static void StartFAF() => Task.Run(() => Start());
-    public static void TickFAF() => Task.Run(() => Tick());
-  
-    static void Beep(int hz = 999, int ms = 999) => NativeMethods.BeepIf(hz, ms);
-    static void BeepFAF(int hz = 999, int ms = 999) => Task.Run(() => NativeMethods.BeepIf(hz, ms));
-
-    const int _5kHz = 5000, _6kHz = 6000, _7kHz = 7000, _8kHz = 8000, _msA = 50, _msB = 150;
+    NativeMethods.BeepIf(_6kHz, 200);
+    NativeMethods.BeepIf(_6kHz, 200);
+    NativeMethods.BeepIf(_6kHz, 200);
+    NativeMethods.BeepIf(_5kHz, 800);
   }
+
+  public static void StartFAF() => Task.Run(Start);
+  public static void FinishFAF() => Task.Run(Finish);
+  public static void ClickFAF() => Task.Run(Click);
+  public static void TickFAF() => Task.Run(Tick);
+  public static void WarnFaF() => Task.Run(Warn);
+  public static void ErrorFaF() => Task.Run(Error);
+
+  static void Beep(int hz = 999, int ms = 999) => NativeMethods.BeepIf(hz, ms);
+  static void BeepFAF(int hz = 999, int ms = 999) => Task.Run(() => NativeMethods.BeepIf(hz, ms));
+
+  const int _5kHz = 5000, _6kHz = 6000, _7kHz = 7000, _8kHz = 8000, _msA = 50, _msB = 150;
 }
