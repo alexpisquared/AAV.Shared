3f6a0d5fc1efb2459ad52a34cf18b003bcb95303
AlexPi <alex.pigida@outlook.com>
2022-12-26 16:16:57 -0500

    namespace StandardLib.Services.BackgroundTask

--- Src/Net.CI/StandardLib/Usings.cs
+++ Src/Net.CI/StandardLib/Usings.cs
@@ -8,10 +8,11 @@ global using System.Reflection;
 global using System.Runtime.CompilerServices;
 global using System.Runtime.InteropServices;
 global using System.Text;
+global using System.Threading;
 global using System.Threading.Tasks;
-global using StandardLib.Extensions;
-global using StandardLib.Helpers;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.Logging;
+global using StandardLib.Extensions;
+global using StandardLib.Helpers;
 //obal using System.Text.Json; //tu: new and very performant Json lib (Dec 2021)
 global using static System.Diagnostics.Trace;
