341b4d136d64f9bdbcb34ead737895b39abc530f
AlexPi <alex.pigida@outlook.com>
2022-12-26 16:42:36 -0500

    ++

--- Src/Net.CI/StandardLib/Services/BackgroundTask.cs
+++ Src/Net.CI/StandardLib/Services/BackgroundTask.cs
@@ -3,10 +3,14 @@ public class BackgroundTask //tu: Nick Chapsas - Scheduling repeating tasks with
 {
   readonly CancellationTokenSource _cts = new();
   readonly PeriodicTimer _timer;
+  readonly Action _act;
   Task? _timerTask;
-  public BackgroundTask(TimeSpan interval) => _timer = new PeriodicTimer(interval);
+
+  //public BackgroundTask(TimeSpan interval) => _timer = new PeriodicTimer(interval);
+  public BackgroundTask(TimeSpan interval, Action act) => (_timer, _act) = (new PeriodicTimer(interval), act);
 
   public void Start(Action act) => _timerTask = DoWorkAsync(act);
+  public void Start() => _timerTask = DoWorkAsync(_act);
   public async Task StopAsync()
   {
     if (_timerTask is null) return;
