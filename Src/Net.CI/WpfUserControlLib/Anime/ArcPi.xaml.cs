ed2596c71348d2f3e5d643c22ff9afa66d24263f
AlexPi <alex.pigida@outlook.com>
2022-12-28 16:40:31 -0500

    ArcPi

--- Src/Net.CI/WpfUserControlLib/Anime/ArcPi.xaml.cs
+++ Src/Net.CI/WpfUserControlLib/Anime/ArcPi.xaml.cs
@@ -3,6 +3,7 @@
 public partial class ArcPi : UserControl
 {
   public ArcPi() => InitializeComponent();
+  public static readonly DependencyProperty ColorProperty = DependencyProperty.Register("Color", typeof(Brush), typeof(ArcPi), new PropertyMetadata(Brushes.Green)); public Brush Color { get => (Brush)GetValue(ColorProperty); set => SetValue(ColorProperty, value); }
   public static readonly DependencyProperty RadiuProperty = DependencyProperty.Register("Radiu", typeof(double), typeof(ArcPi), new PropertyMetadata(50d)); public double Radiu { get => (double)GetValue(RadiuProperty); set => SetValue(RadiuProperty, value); }
   public static readonly DependencyProperty AngleProperty = DependencyProperty.Register("Angle", typeof(double), typeof(ArcPi), new PropertyMetadata(0d, PropertyChangedCallback)); public double Angle { get => (double)GetValue(AngleProperty); set => SetValue(AngleProperty, value); }
   static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((ArcPi)d).DrawArc((double)e.NewValue);
