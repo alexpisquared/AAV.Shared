790cd4ded7f57b2c35b047d2f86c056a98962a94
AlexPi <alex.pigida@outlook.com>
2022-12-31 16:09:40 -0500

    pau

--- Src/Net.CI/Ambience/SpeechSynth.cs
+++ Src/Net.CI/Ambience/SpeechSynth.cs
@@ -1,32 +1,39 @@
 ﻿namespace AmbienceLib;
-public enum VMode { Unknown, Prosody, Express }
-
 public class SpeechSynth : IDisposable
 {
-  const string _voiceNameFallback = "en-GB-SoniaNeural", _rgn = "canadacentral", _key = "use proper key here", _pathToCache = @"c:\temp\tts\";
+  const string _voiceNameFallback = "uk-UA-PolinaNeural", _rgn = "canadacentral", _key = "use proper key here", _pathToCache = @"c:\temp\tts\";
   readonly string _speechKey;
   readonly SpeechSynthesizer _synthesizer;
-  bool _disposedValue;
+  bool _disposedValue, useCached = true;
 
   public SpeechSynth(string speechKey)
   {
-    _speechKey = speechKey;
+    var speechConfig = SpeechConfig.FromSubscription(_speechKey = speechKey, "canadacentral");
 
-    var speechConfig = SpeechConfig.FromSubscription(_speechKey, "canadacentral");
-
-    speechConfig.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Riff24Khz16BitMonoPcm);
-
-    _synthesizer = new SpeechSynthesizer(speechConfig, null);
-    //_synthesizer = new SpeechSynthesizer(speechConfig);
+    if (useCached)
+    {
+      speechConfig.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Riff24Khz16BitMonoPcm);
+      _synthesizer = new SpeechSynthesizer(speechConfig, null);
+    }
+    else
+      _synthesizer = new SpeechSynthesizer(speechConfig);
   }
 
   public async Task SpeakAsync(string msg)
   {
-    var filename = @$"{_pathToCache}{RemoveIllegalCharacters(RemoveIllegalCharacters(msg))}.wav";
-    if (!File.Exists(filename))
-      await SynthesizeAudioAsync(msg, filename);
+    if (useCached)
+    {
 
-    PlayWavFileAsync(filename);
+      var filename = @$"{_pathToCache}{RemoveIllegalCharacters(RemoveIllegalCharacters(msg))}.wav";
+      if (!File.Exists(filename))
+        await SynthesizeAudioAsync(msg, filename);
+
+      PlayWavFileAsync(filename);
+    }
+    else
+    {
+      await SynthesizeAudioAsync(msg);
+    }
   }
   public async Task SpeakProsodyAsync(string msg, string voiceName = _voiceNameFallback, double speakingRate = 1.25)
   {
@@ -36,14 +43,34 @@ public class SpeechSynth : IDisposable
 
     PlayWavFileAsync(filename);
   }
-  public async Task SpeakExpressAsync(string msg, string voiceName = _voiceNameFallback, string styleForExpressOnly = "cheerful")
+  public async Task SpeakExpressAsync(string msg, string voiceName = _voiceNameFallback, string style = "cheerful")
   {
-    var filename = @$"{_pathToCache}{RemoveIllegalCharacters(voiceName)}~{RemoveIllegalCharacters(styleForExpressOnly)}~{RemoveIllegalCharacters(msg)}.wav";
+    var filename = @$"{_pathToCache}{RemoveIllegalCharacters(voiceName)}~{RemoveIllegalCharacters(style)}~{RemoveIllegalCharacters(msg)}.wav";
     if (!File.Exists(filename))
-      await SynthesizeAudioExpressAsync(msg, voiceName, styleForExpressOnly, filename);
+      await SynthesizeAudioExpressAsync(msg, voiceName, style, filename);
 
     PlayWavFileAsync(filename);
   }
+
+  async Task SynthesizeAudioAsync(string msg, string filename)
+  {
+    try
+    {
+      var result = await _synthesizer.SpeakTextAsync(msg);
+      using var stream = AudioDataStream.FromResult(result);
+      await stream.SaveToWaveFileAsync(filename);
+    }
+    catch (Exception ex) { WriteLine($"■■■ {ex.Message}"); if (Debugger.IsAttached) Debugger.Break(); else throw; }
+  }
+  async Task SynthesizeAudioAsync(string msg)
+  {
+    try
+    {
+      var result = await _synthesizer.SpeakTextAsync(msg);
+    }
+    catch (Exception ex) { WriteLine($"■■■ {ex.Message}"); if (Debugger.IsAttached) Debugger.Break(); else throw; }
+  }
+
   async Task SynthesizeAudioProsodyAsync(string msg, string voice, double speakingRate, string filename)
   {
     try
@@ -73,14 +100,14 @@ public class SpeechSynth : IDisposable
     catch (Win32Exception) { throw; }
     catch (Exception) { throw; }
   }
-  async Task SynthesizeAudioExpressAsync(string msg, string voice, string styleForExpressOnly, string filename)
+  async Task SynthesizeAudioExpressAsync(string msg, string voice, string style, string filename)
   {
     try
     {
       var lang = voice.Length > 5 ? voice[..5] : "en-US";
 
       using var result = await _synthesizer.SpeakSsmlAsync(
-        $@" <speak version=""1.0"" xmlns=""https://www.w3.org/2001/10/synthesis"" xml:lang=""{lang}"" xmlns:mstts=""https://www.w3.org/2001/mstts""><voice name=""{voice}""><mstts:express-as style=""{styleForExpressOnly}"" styledegree=""2"" >{msg}</mstts:express-as></voice></speak>");
+        $@" <speak version=""1.0"" xmlns=""https://www.w3.org/2001/10/synthesis"" xml:lang=""{lang}"" xmlns:mstts=""https://www.w3.org/2001/mstts""><voice name=""{voice}""><mstts:express-as style=""{style}"" styledegree=""2"" >{msg}</mstts:express-as></voice></speak>");
 
       if (result.Reason == ResultReason.Canceled)
       {
@@ -98,67 +125,13 @@ public class SpeechSynth : IDisposable
       Write("\n");
 
       using var stream = AudioDataStream.FromResult(result);
-      await stream.SaveToWaveFileAsync(filename);
+
+      var temp = Path.GetTempFileName();
+      await stream.SaveToWaveFileAsync(temp);
+      File.Move(temp, filename);
     }
-    catch (Win32Exception) { throw; }
-    catch (Exception) { throw; }
+    catch (Exception ex) { WriteLine($"■■■ {ex.Message}"); if (Debugger.IsAttached) Debugger.Break(); else throw; }
   }
-  async Task SynthesizeAudioAsync(string tts, string filename)
-  {
-    var result = await _synthesizer.SpeakTextAsync(tts);
-
-    using var stream = AudioDataStream.FromResult(result);
-    await stream.SaveToWaveFileAsync(filename);
-  }
-
-  //public async Task SpeakOldPocOnly(string msg, VMode vmode = VMode.Prosody, string? voice = null, string? styleForExpressOnly = null)
-  //{
-  //  try
-  //  {
-  //    //    std voices from https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#standard-voices
-  //    // neural voices from https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#neural-voices
-
-  //    var speakingStyles = new[] { // https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/speech-synthesis-markup?tabs=csharp#adjust-speaking-styles
-  //         "angry" ,         // XiaoxiaoNeural only
-  //         "sad" ,           // XiaoxiaoNeural only
-  //         "affectionate",   // XiaoxiaoNeural only
-  //         "newscast-formal" ,  // AriaNeural only 
-  //         "newscast-casual" ,  // AriaNeural only 
-  //         "customerservice" ,  // AriaNeural only 
-  //         "chat"            ,  // AriaNeural only 
-  //         "cheerful"        ,  // AriaNeural only 
-  //         "empathetic"     };  // AriaNeural only 
-
-  //    var sStyle =
-  //      styleForExpressOnly == "random" ? speakingStyles[_rnd.Next(speakingStyles.Length)] :
-  //      speakingStyles.Contains(styleForExpressOnly) ? styleForExpressOnly :
-  //      "chat";
-
-  //    var voiceName = voice ?? _voiceNameFallback;
-  //    var lang = "en-US"; // voice.Length > 5 ? voice.Substring(0, 5) : "en-GB";
-  //    var sw = Stopwatch.StartNew();
-  //    using var result = await _synthesizer.SpeakSsmlAsync(vmode == VMode.Prosody ?
-  //      $@" <speak version=""1.0"" xmlns=""https://www.w3.org/2001/10/synthesis"" xml:lang=""{lang}""                                              ><voice name=""{voiceName}""><prosody rate=""{1.75}""                      >{msg}</prosody></voice></speak>" : //todo: rate does not work below:
-  //      $@" <speak version=""1.0"" xmlns=""https://www.w3.org/2001/10/synthesis"" xml:lang=""{lang}"" xmlns:mstts=""https://www.w3.org/2001/mstts""><voice name=""{voiceName}""><mstts:express-as style=""{sStyle}"" styledegree=""2"" >{msg}</mstts:express-as></voice></speak>");
-
-  //    if (result.Reason == ResultReason.Canceled)
-  //    {
-  //      var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
-  //      Write($"\tCANCELED: {cancellation.Reason}");
-
-  //      if (cancellation.Reason == CancellationReason.Error)
-  //      {
-  //        Write($"  Error: {cancellation.ErrorCode}-{cancellation.ErrorDetails}");
-  //      }
-  //    }
-  //    else
-  //      Write($"  result: '{result.Reason}'");
-
-  //    Write("\n");
-  //  }
-  //  catch (Win32Exception) { throw; }
-  //  catch (Exception) { throw; }
-  //}
 
   void PlayWavFileAsync(string filename) => new SoundPlayer(filename).PlaySync();//_player.SoundLocation= filename;//_player.Play();
   string RemoveIllegalCharacters(string filename = "My:File*Name?")
@@ -204,10 +177,14 @@ https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/langua
 public record Voice_styles_and_roles(string Voice, string[] Styles, string[] Roles);
 public class CC
 {
-  public static Voice_styles_and_roles EngbRyanNeural { get; } = new("en-GB-RyanNeural", new[] { cheerful, chat }, new string[] { });
-  public static Voice_styles_and_roles EngbSoniaNeural = new("en-GB-SoniaNeural", new[] { cheerful, sad }, new string[] { });
-  public static Voice_styles_and_roles EnusAriaNeural = new("en-US-AriaNeural", new[] { chat, customerservice, narration_professional, newscast_casual, newscast_formal, cheerful, empathetic, angry, sad, excited, friendly, terrified, shouting, unfriendly, whispering, hopeful }, new string[] { });
-  public static Voice_styles_and_roles ZhcnXiaomoNeural = new("zh-CN-XiaomoNeural", new[] { embarrassed, calm, fearful, cheerful, disgruntled, serious, angry, sad, depressed, affectionate, gentle, envious }, new[] { YoungAdultFemale, YoungAdultMale, OlderAdultFemale, OlderAdultMale, SeniorFemale, SeniorMale, Girl, Boy });
+  public static Voice_styles_and_roles
+    UkuaOstapNeural = new("uk-UA-OstapNeural", new string[] { }, new string[] { }),
+    UkuaPolinaNeural = new("uk-UA-PolinaNeural", new string[] { }, new string[] { }),
+    EngbRyanNeural = new("en-GB-RyanNeural", new[] { cheerful, chat }, new string[] { }),
+    EngbSoniaNeural = new("en-GB-SoniaNeural", new[] { cheerful, sad }, new string[] { }),
+    EnusAriaNeural = new("en-US-AriaNeural", new[] { chat, customerservice, narration_professional, newscast_casual, newscast_formal, cheerful, empathetic, angry, sad, excited, friendly, terrified, shouting, unfriendly, whispering, hopeful }, new string[] { }),
+    ZhcnXiaomoNeural = new("zh-CN-XiaomoNeural", new[] { embarrassed, calm, fearful, cheerful, disgruntled, serious, angry, sad, depressed, affectionate, gentle, envious }, new[] { YoungAdultFemale, YoungAdultMale, OlderAdultFemale, OlderAdultMale, SeniorFemale, SeniorMale, Girl, Boy });
+
   const string
     embarrassed = "embarrassed",
     calm = "calm",
