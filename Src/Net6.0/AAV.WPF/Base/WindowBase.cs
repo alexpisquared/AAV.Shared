ff9dbfcdc229c6eb904a1b054147d4fe0297a17b
AlexPi <alex.pigida@outlook.com>
2023-01-15 21:55:41 -0500

    +++++

--- Src/Net6.0/AAV.WPF/Base/WindowBase.cs
+++ Src/Net6.0/AAV.WPF/Base/WindowBase.cs
@@ -3,18 +3,18 @@
 using Microsoft.Extensions.Logging;
 //using AAV.Sys.Core3Ext;
 #endif
-using AAV.Sys.Ext;
-using AAV.Sys.Helpers;
-using AAV.WPF.Ext;
 using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
-using System.IO;
+using AAV.Sys.Ext;
+using AAV.Sys.Helpers;
+using AAV.WPF.Ext;
 
 namespace AAV.WPF.Base
 {
@@ -27,7 +27,7 @@ namespace AAV.WPF.Base
 
     protected bool IgnoreEscape { get; set; }
     protected bool IgnoreWindowPlacement { get; set; } = false;
-    string IsoFilenameONLY => /*$"{GetType().Name}.xml";*/ Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), @$"AppSettings\{AppDomain.CurrentDomain.FriendlyName}\{GetType().Name}.json");
+    string IsoFilenameONLY => /*$"{GetType().Name}.xml";*/ Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), @$"AppSettings\{AppDomain.CurrentDomain.FriendlyName}\{GetType().Name}{(IsDbg ? ".dbg" : "")}.json");
 
 #if MockingCore3
     class Logger
@@ -58,9 +58,8 @@ namespace AAV.WPF.Base
       //  //e.Handled = true; 
       //};
 
-
       //useDoubleClick += (s, e) => WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal; <= too obnoxious (Jan2020)
-      MouseWheel += (s, e) => { if (!(Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))) return; ZV += (e.Delta * .001); e.Handled = true; Debug.WriteLine(Title = $">>ZV:{ZV}"); }; //tu:
+      MouseWheel += (s, e) => { if (!(Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))) return; ZV += e.Delta * .001; e.Handled = true; Debug.WriteLine(Title = $">>ZV:{ZV}"); }; //tu:
 
       //Loaded += (s, e) => { applyTheme(Thm); };
       KeyUp += (s, e) =>
@@ -117,6 +116,7 @@ namespace AAV.WPF.Base
 
           Application.Current.Resources.MergedDictionaries.Add(dict);
         }
+
         Thm = themeName;
 
         //~Application.Current.Resources.MergedDictionaries.ToList().ForEach(r => Trace.WriteLine($"    ~> ++ Adding:   {((System.Windows.Markup.IUriContext)r)?.BaseUri?.AbsolutePath.Replace(pref, "..."/*, StringComparison.OrdinalIgnoreCase*/)}"));
@@ -145,11 +145,11 @@ namespace AAV.WPF.Base
 
           ApplyTheme(string.IsNullOrEmpty(wpc.Thm) ? _defaultTheme : wpc.Thm); // -- for Mail.sln - causes the error: Cannot find resource named 'WindowStyle_Aav0'. Resource names are case sensitive 
         }
-        catch (Exception ex) { ex?.Log(); ZV = 1d; winPlcmnt = JsonFileSerializer.Load<NativeMethods.WindowPlacement>(IsoFilenameONLY); }
+        catch (Exception ex) { _ = (ex?.Log()); ZV = 1d; winPlcmnt = JsonFileSerializer.Load<NativeMethods.WindowPlacement>(IsoFilenameONLY); }
 
         winPlcmnt.length = Marshal.SizeOf(typeof(NativeMethods.WindowPlacement));
         winPlcmnt.flags = 0;
-        winPlcmnt.showCmd = (winPlcmnt.showCmd == _swShowMinimized ? _swShowNormal : winPlcmnt.showCmd);
+        winPlcmnt.showCmd = winPlcmnt.showCmd == _swShowMinimized ? _swShowNormal : winPlcmnt.showCmd;
 
         if (winPlcmnt.normalPosition.Bottom == 0 && winPlcmnt.normalPosition.Top == 0 && winPlcmnt.normalPosition.Left == 0 && winPlcmnt.normalPosition.Right == 0)
         {
@@ -162,7 +162,7 @@ namespace AAV.WPF.Base
           if (winPlcmnt.normalPosition.Right > SystemParameters.WorkArea.Width)
           {
             winPlcmnt.normalPosition.Left = _margin;
-            winPlcmnt.normalPosition.Top += (_margin + (int)ActualHeight);
+            winPlcmnt.normalPosition.Top += _margin + (int)ActualHeight;
             _currentTop += (int)ActualHeight;
             winPlcmnt.normalPosition.Right = winPlcmnt.normalPosition.Left + (int)ActualWidth;
             _currentLeft = _margin;
@@ -181,16 +181,23 @@ namespace AAV.WPF.Base
         //else
         //  Trace.WriteLine($"{_isoFilenameONLY,20}: Window Positions NOT all zeros: btm:{winPlcmnt.normalPosition.Bottom,-4} top:{winPlcmnt.normalPosition.Top,-4} lft:{winPlcmnt.normalPosition.Left,-4} rht:{winPlcmnt.normalPosition.Right,-4}.  {SystemParameters.WorkArea.Width}x{SystemParameters.WorkArea.Height} is this the screen dims?");
 
-        NativeMethods.SetWindowPlacement_(new WindowInteropHelper(this).Handle, ref winPlcmnt); //Note: if window was closed on a monitor that is now disconnected from the computer, SetWindowPlacement will place the window onto a visible monitor.
+        _ = NativeMethods.SetWindowPlacement_(new WindowInteropHelper(this).Handle, ref winPlcmnt); //Note: if window was closed on a monitor that is now disconnected from the computer, SetWindowPlacement will place the window onto a visible monitor.
       }
-      catch (Exception ex) { ex.Log($"*&^> for {this.GetType().Name}."); }
+      catch (Exception ex) { _ = ex.Log($"*&^> for {GetType().Name}."); }
     }
     protected override void OnClosing(CancelEventArgs e) // WARNING - Not fired when Application.SessionEnding is fired
     {
       base.OnClosing(e);
 
-      NativeMethods.GetWindowPlacement_(new WindowInteropHelper(this).Handle, out var wp);
+      _ = NativeMethods.GetWindowPlacement_(new WindowInteropHelper(this).Handle, out var wp);
       JsonFileSerializer.Save(new NativeMethods.WPContainer { WindowPlacement = wp, Zb = ZV, Thm = Thm }, IsoFilenameONLY);
     }
+    bool IsDbg =>
+#if DEBUG
+        true;
+#else
+        false;
+#endif
+
   }
 }
