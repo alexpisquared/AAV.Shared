11103c1ab893908ec8f8ad26bc0ee1373277ed4a
AlexPi <alex.pigida@outlook.com>
2023-03-19 11:32:10 -0400

    ++

---
+++ Src/NetLts/AmbienceDevDbg/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+do
+{
+  await new AmbienceLib.Bpr().ErrorAsync();
+  //await AmbienceLib.Bpr.DevDbg();
+} while (Console.ReadKey().Key != ConsoleKey.Q);
