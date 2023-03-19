f1d1f6d97162c40ef6eefc67f595613233e85063
AlexPi <alex.pigida@outlook.com>
2023-03-19 19:49:30 -0400

    EOWend

--- Src/NetLts/WpfUserControlLib/Helpers/ConfigHelper.cs
+++ Src/NetLts/WpfUserControlLib/Helpers/ConfigHelper.cs
@@ -60,7 +60,8 @@ public class ConfigHelper //todo: appsettings as user prefs: https://makolyte.co
     cfg[CfgName.WhereAmAy] = "HardCODE in .\\WpfUserControlLib\\Helpers\\ConfigHelper.cs";
     cfg[CfgName.ServerLst] = sqlServerCSV ?? StandardLib.Base.Consts.SqlServerCSV;
     cfg[CfgName.DtBsNmLst] = dtBsNameCSV ?? StandardLib.Base.Consts.DtBsNameCSV;
-    cfg[CfgName.LogFolder] = VersionHelper.IsDbg ? @"C:\Temp\Logs\..log" : @"Z:\felixdev\apps\data\Oleksa\Tooling\SmreV2\Logs\..log";
+    cfg[CfgName.LogFolder] = //VersionHelper.IsDbg ? @"C:\Temp\Logs\..log" : 
+      @"Z:\felixdev\apps\data\Oleksa\Tooling\SmreV2\Logs\..log";
     cfg[CfgName.SqlVerIpm] = VersionHelper.IsDbg ? "Server={0};Database={1};Trusted_Connection=True;Encrypt=False;Connection Timeout=15;" : "Server={0};Database={1};Trusted_Connection=True;Encrypt=False;Connection Timeout=52;"; // On dbg 5 sec was fine .. let's see if 1 sec is going to cause YY's issues.
     cfg[CfgName.SqlVerIp_] = "Server={0};Database={1};persist security info=True;user id={2};password={3};MultipleActiveResultSets=True;App=EntityFramework;Connection Timeout=57";
 
