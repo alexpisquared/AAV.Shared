5578cff96855f00d5ab82569e13d870edd99827c
AlexPi <alex.pigida@outlook.com>
2023-03-31 13:46:29 -0400

    9+++

--- Src/NetLts/WinFormsControlLib/WinFormHelper.cs
+++ Src/NetLts/WinFormsControlLib/WinFormHelper.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Linq;
-using System.Windows.Forms;
+﻿namespace WinFormsControlLib;
 
-namespace WindowsFormsLib
+public class WinFormHelper
 {
-  public class WinFormHelper
-  {
-    public static Screen[] GetAllScreens() => Screen.AllScreens;
-    public static Screen LargestScreen => Screen.AllScreens.First(r => r.WorkingArea.Width ==(Screen.AllScreens.Max(r => r.WorkingArea.Width)));
-    public static Screen PrimaryScreen => Screen.PrimaryScreen ?? throw new ArgumentNullException("▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀");
-    public static Screen SecondaryScreen => Screen.AllScreens.First(r => r != Screen.PrimaryScreen);
-  }
+  public static Screen[] GetAllScreens() => Screen.AllScreens;
+  public static Screen LargestScreen => Screen.AllScreens.First(r => r.WorkingArea.Width == Screen.AllScreens.Max(r => r.WorkingArea.Width));
+  public static Screen PrimaryScreen => Screen.PrimaryScreen ?? throw new ArgumentNullException("▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀");
+  public static Screen SecondaryScreen => Screen.AllScreens.First(r => r != Screen.PrimaryScreen);
 
-  static class Program {[STAThread] static void Main___() { } } //?? what is this doing here?..
+  public static Rectangle GetFirstNonPrimaryScreenBounds()
+  {
+    var secondaryScreen = Screen.AllScreens.FirstOrDefault(s => !s.Primary);
+    return (secondaryScreen is null) ? new Rectangle(0, 0, 1920, 1080) : secondaryScreen.Bounds;
+  }
 }
+
+static class Program { [STAThread] static void Main___() { } } //?? what is this doing here?..
