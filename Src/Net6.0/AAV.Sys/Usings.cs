01ea441e7b1141f6ace1b79fc2591c83cca3e4d7
alexpisqueared <alex.pigida@outlook.com>
2023-04-21 10:14:29 -0400

    look and feel finalization; tracing EOScrSvr log entryies.

---
+++ Src/Net6.0/AAV.Sys/Usings.cs
@@ -0,0 +1,10 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+//obal using System.Text.Json; //tu: new and very performant Json lib (Dec 2021)
+global using System.IO;
+global using System.Linq;
+global using System.Reflection;
+global using System.Threading.Tasks;
+global using AAV.Sys.Ext;
+global using AAV.Sys.Helpers;
