0128e58e19298982a63dd104b92837c48351f849
alexpisqueared <alex.pigida@outlook.com>
2023-06-21 16:06:53 -0400

    Union, Prim, Scnd

--- Src/NetLts/WinFormsControlLib/WinFormHelper.cs
+++ Src/NetLts/WinFormsControlLib/WinFormHelper.cs
@@ -2,7 +2,7 @@
 
 public class WinFormHelper
 {
-  public static Screen[] GetAllScreens() => Screen.AllScreens;
+  public static Screen[] GetAllScreens => Screen.AllScreens;
   public static Screen LargestScreen => Screen.AllScreens.First(r => r.WorkingArea.Width == Screen.AllScreens.Max(r => r.WorkingArea.Width));
   public static Screen PrimaryScreen => Screen.PrimaryScreen ?? throw new ArgumentNullException("▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀");
   public static Screen SecondaryScreen => Screen.AllScreens.First(r => r != Screen.PrimaryScreen);
@@ -12,4 +12,5 @@ public class WinFormHelper
     var secondaryScreen = Screen.AllScreens.FirstOrDefault(s => !s.Primary);
     return (secondaryScreen is null) ? new Rectangle(0, 0, 1920, 1080) : secondaryScreen.Bounds;
   }
+  public static Rectangle GetSumOfAllBounds => Rectangle.Union(PrimaryScreen.Bounds, SecondaryScreen.Bounds);
 }
