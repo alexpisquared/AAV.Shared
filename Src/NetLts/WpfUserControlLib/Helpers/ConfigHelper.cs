140f642ef4889df71734745735a3b2a4b322a61a
alexpisqueared <alex.pigida@outlook.com>
2023-06-29 11:05:31 -0400

    Update ConfigHelper.cs

--- Src/NetLts/WpfUserControlLib/Helpers/ConfigHelper.cs
+++ Src/NetLts/WpfUserControlLib/Helpers/ConfigHelper.cs
@@ -132,3 +132,30 @@ public class ConfigHelper //todo: appsettings as user prefs: https://makolyte.co
 }}";
   class WhatIsThatForType { public string MyProperty { get; set; } = "<Default Value of Nothing Special>"; }
 }
+
+public class ConfigRandomizer
+{
+  readonly Random _random = new(DateTime.Now.Millisecond);
+  readonly IConfigurationRoot _config;
+
+  public ConfigRandomizer(string jsonFile = "appsettings.AlexPi.Scr.json") => _config = new ConfigurationBuilder()
+      .SetBasePath(AppContext.BaseDirectory)
+      .AddJsonFile(jsonFile)  // the last overwrites!!!
+      .AddUserSecrets<ConfigRandomizer>()          // the last overwrites!!! <== <== <== <== <== <== <== 
+      .Build();
+
+  public IConfigurationRoot Config => _config;
+  public string GetValue(string name) => _config[name] ?? "";
+  public string GetRandomFromUserSection(string section)
+  {
+    var sn = $"{section}_{Environment.UserName}";
+    var sa = _config.GetSection(sn).Get<string[]>();
+    if (sa == null)
+    {
+      WriteLine($"\"{sn}\": [\"abc\", \"efg\", \"hij\"],      // <== if (sc?.Value == null)");
+      return $"Section  '{section}'  not found in app settings nor secrets";
+    }
+
+    return sa[_random.Next(sa.Length)];
+  }
+}
