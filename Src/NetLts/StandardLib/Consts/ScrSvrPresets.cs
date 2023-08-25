a11ead4c29ac59ae11f531212e388930e3d138f0
alexpisqueared <alex.pigida@outlook.com>
2023-08-25 19:35:09 -0400

    sleep pc generalisation

---
+++ Src/NetLts/StandardLib/Consts/ScrSvrPresets.cs
@@ -0,0 +1,10 @@
+﻿namespace StandardLib.Consts;
+public class ScrSvrPresets
+{
+  public static double MinToPcSleep =>
+     Environment.MachineName == "BEELINK1" ? 16 : // see the logs if 10 min limit kicks in here
+     Environment.MachineName == "RAZER1" ? 56 :   // 1hr for: PerfectMind registration, etc....
+     Environment.MachineName == "ASUS2" ? 16 :    // see the logs if 10 min limit kicks in here
+     Environment.MachineName == "YOGA1" ? 16 :    // see the logs if 10 min limit kicks in here
+     8; //todo: something closes the app exactly on 10 min mark?!?!?!?! => keep it under 10 > 9.5=8+1+.5
+}
