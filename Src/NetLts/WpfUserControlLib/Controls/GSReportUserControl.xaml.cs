983db7d699c938811aef9e0160a112353e4b202a
alexpisqueared <alex.pigida@outlook.com>
2024-01-15 14:48:09 -0500

    GSReport user control

---
+++ Src/NetLts/WpfUserControlLib/Controls/GSReportUserControl.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace WpfUserControlLib.Controls;
+public partial class GSReportUserControl : UserControl
+{
+  public GSReportUserControl() => InitializeComponent();
+  public static readonly DependencyProperty GSReportProperty = DependencyProperty.Register("GSReport", typeof(string), typeof(GSReportUserControl), new PropertyMetadata("efg")); public string GSReport { get => (string)GetValue(GSReportProperty); set => SetValue(GSReportProperty, value); }
+  void OnHideGSReport(object s, RoutedEventArgs e) => Opener0.IsChecked = false;
+}
