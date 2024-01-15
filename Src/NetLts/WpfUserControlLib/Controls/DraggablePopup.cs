f4d93d0832f1b1c25fa8cf629b63c11f697cec45
alexpisqueared <alex.pigida@outlook.com>
2024-01-14 20:57:06 -0500

    +++

---
+++ Src/NetLts/WpfUserControlLib/Controls/DraggablePopup.cs
@@ -0,0 +1,44 @@
+﻿namespace WpfUserControlLib.Controls;
+
+public class DraggablePopup : Popup
+{
+  bool _isDragging;
+  Point _initialMousePosition;
+
+  protected override void OnInitialized(EventArgs e)
+  {
+    var contents = Child as FrameworkElement;
+    ArgumentNullException.ThrowIfNull(contents, "■ DraggablePopup either has no content if content that does not derive from FrameworkElement. Must be fixed for dragging to work. ■");
+
+    contents.MouseLeftButtonDown += Child_MouseLeftButtonDown;
+    contents.MouseLeftButtonUp += Child_MouseLeftButtonUp;
+    contents.MouseMove += Child_MouseMove;
+  }
+
+  void Child_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+  {
+    _initialMousePosition = e.GetPosition(null);
+    
+    _ = (sender as FrameworkElement)?.CaptureMouse();
+    _isDragging = true;
+    e.Handled = true;
+  }
+
+  void Child_MouseMove(object sender, MouseEventArgs e)
+  {
+    if (!_isDragging) return;
+
+    var currentPoint = e.GetPosition(null);
+    HorizontalOffset += currentPoint.X - _initialMousePosition.X;
+    VerticalOffset += currentPoint.Y - _initialMousePosition.Y;
+  }
+
+  void Child_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+  {
+    if (!_isDragging) return;
+
+    (sender as FrameworkElement)?.ReleaseMouseCapture();
+    _isDragging = false;
+    e.Handled = true;
+  }
+}
