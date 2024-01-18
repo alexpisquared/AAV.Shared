6b70efc2c6b80432297d100142b7aa0e12495456
alexpisqueared <alex.pigida@outlook.com>
2024-01-18 09:41:50 -0500

    nav svc - no joy

--- Src/NetLts/WpfUserControlLib/Controls/GSReportUserControl.xaml.cs
+++ Src/NetLts/WpfUserControlLib/Controls/GSReportUserControl.xaml.cs
@@ -2,6 +2,6 @@
 public partial class GSReportUserControl : UserControl
 {
   public GSReportUserControl() => InitializeComponent();
-  public static readonly DependencyProperty GSReportProperty = DependencyProperty.Register("GSReport", typeof(string), typeof(GSReportUserControl), new PropertyMetadata("efg")); public string GSReport { get => (string)GetValue(GSReportProperty); set => SetValue(GSReportProperty, value); }
+  public static readonly DependencyProperty GSReportProperty = DependencyProperty.Register("GSReport", typeof(string), typeof(GSReportUserControl), new PropertyMetadata("+efg+")); public string GSReport { get => (string)GetValue(GSReportProperty); set => SetValue(GSReportProperty, value); }
   void OnHideGSReport(object s, RoutedEventArgs e) => Opener0.IsChecked = false;
 }
