16f2434abb2db9f007dd86cc7ca0192ea09269bc
alexpisqueared <alex.pigida@outlook.com>
2024-02-06 09:13:31 -0500

    pau

--- Src/NetLts/WpfUserControlLib/Controls/GSReportUserControl.xaml.cs
+++ Src/NetLts/WpfUserControlLib/Controls/GSReportUserControl.xaml.cs
@@ -1,9 +1,9 @@
-﻿
-namespace WpfUserControlLib.Controls;
+﻿namespace WpfUserControlLib.Controls;
 public partial class GSReportUserControl : UserControl
 {
   public GSReportUserControl() => InitializeComponent();
-  void OnHideGSReport(object s, RoutedEventArgs e) => Opener0.IsChecked = false;
+  void OnHideGSReport(object s, RoutedEventArgs e) { GSReport = ""; Opener0.IsChecked = false; }
+
   public static readonly DependencyProperty GSReportProperty = DependencyProperty.Register("GSReport", typeof(string), typeof(GSReportUserControl), new PropertyMetadata("+efg+", PropertyCnanged)); public string GSReport { get => (string)GetValue(GSReportProperty); set => SetValue(GSReportProperty, value); }
   static void PropertyCnanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => (d as GSReportUserControl)?.ScrollViewer1.ScrollToEnd();
 }
