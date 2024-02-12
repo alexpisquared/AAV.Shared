ad555f178316b6bf2361569adddf233d976d0317
alexpisqueared <alex.pigida@outlook.com>
2024-02-12 09:16:52 -0500

    ++

--- Src/NetLts/StandardContractsLib/ISpeechSynth.cs
+++ Src/NetLts/StandardContractsLib/ISpeechSynth.cs
@@ -6,8 +6,8 @@ public interface ISpeechSynth
 
   void Dispose();
   void SpeakAsyncCancelAll();
-  Task SpeakAsync(string msg, double speakingRate = 1, double volumePercent = 33, string voice = "", string style = "friendly", string role = "YoungAdultFemale");
   void SpeakFAF(string msg, double speakingRate = 1, double volumePercent = 33, string voice = "", string style = "friendly");
-  void SpeakFreeFAF(string msg, int speakingRate = 3, int volumePercent = 33, string voice = _voice);    // does not use the Azure Cognitive Services.
-  Task SpeakFreeAsync(string msg, int speakingRate = 3, int volumePercent = 33, string voice = _voice);  // does not use the Azure Cognitive Services.
+  Task SpeakAsync(string msg, double speakingRate = 1, double volumePercent = 33, string voice = "", string style = "friendly", string role = "YoungAdultFemale");
+  void SpeakFreeFAF(string msg, int speakingRate1010 = 3, int volumePercent = 33, string voice = _voice);    // does not use the Azure Cognitive Services.
+  Task SpeakFreeAsync(string msg, int speakingRate1010 = 3, int volumePercent = 33, string voice = _voice);  // does not use the Azure Cognitive Services.
 }
