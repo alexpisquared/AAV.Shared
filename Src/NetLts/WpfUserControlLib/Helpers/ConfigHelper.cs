02179708ec4efceeb33f090123004350fb163ad8
alexpisqueared <alex.pigida@outlook.com>
2024-04-13 17:41:00 -0400

    pau

--- Src/NetLts/WpfUserControlLib/Helpers/ConfigHelper.cs
+++ Src/NetLts/WpfUserControlLib/Helpers/ConfigHelper.cs
@@ -134,30 +134,3 @@ public class ConfigHelper //todo: appsettings as user prefs: https://makolyte.co
 }
   
 class WhatIsThatForType { public string MyProperty { get; set; } = "<Default Value of Nothing Special>"; }
-
-public class ConfigRandomizer
-{
-  readonly Random _random = new(DateTime.Now.Millisecond);
-  readonly IConfigurationRoot _config;
-
-  public ConfigRandomizer(string jsonFile = "appsettings.json") => _config = new ConfigurationBuilder()
-      .SetBasePath(AppContext.BaseDirectory)
-      //.AddJsonFile(jsonFile)                                            // the last overwrites!!!
-      .AddUserSecrets<WhatIsThatForType>() //note: of this assenbly!!!    // the last overwrites!!!
-      .Build();
-
-  public IConfigurationRoot Config => _config;
-  public string GetValue(string name) => _config[name] ?? "";
-  public string GetRandomFromUserSection(string section)
-  {
-    var sn = $"{section}_{Environment.UserName}";
-    var sa = _config.GetSection(sn).Get<string[]>();
-    if (sa == null)
-    {
-      WriteLine($"\"{sn}\": [\"abc\", \"efg\", \"hij\"],      // <== if (sc?.Value == null)");
-      return $"Section  '{section}'  not found in app settings nor secrets";
-    }
-
-    return sa[_random.Next(sa.Length)];
-  }
-}
---
+++ Src/NetLts/WpfUserControlLib/Helpers/ConfigRandomizer.cs
@@ -0,0 +1,28 @@
+﻿namespace WpfUserControlLib.Helpers;
+
+public class ConfigRandomizer
+{
+  readonly Random _random = new(DateTime.Now.Millisecond);
+  readonly IConfigurationRoot _config;
+
+  public ConfigRandomizer(string jsonFile = "appsettings.json") => _config = new ConfigurationBuilder()
+      .SetBasePath(AppContext.BaseDirectory)
+      //.AddJsonFile(jsonFile)                                            // the last overwrites!!!
+      .AddUserSecrets<WhatIsThatForType>() //note: of this assenbly!!!    // the last overwrites!!!
+      .Build();
+
+  public IConfigurationRoot Config => _config;
+  public string GetValue(string name) => _config[name] ?? "";
+  public string GetRandomFromUserSection(string section)
+  {
+    var sn = $"{section}_{Environment.UserName}";
+    var sa = _config.GetSection(sn).Get<string[]>();
+    if (sa == null)
+    {
+      WriteLine($"\"{sn}\": [\"abc\", \"efg\", \"hij\"],      // <== if (sc?.Value == null)");
+      return $"Section  '{section}'  not found in app settings nor secrets";
+    }
+
+    return sa[_random.Next(sa.Length)];
+  }
+}
