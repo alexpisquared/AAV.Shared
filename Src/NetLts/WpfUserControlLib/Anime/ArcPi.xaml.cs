94aa5ab02beed1d4a97b05f97956e5f5685509ac
alexpisqueared <alex.pigida@outlook.com>
2024-12-23 21:48:58 -0500

    [main] pau

--- Src/NetLts/WpfUserControlLib/Anime/ArcPi.xaml.cs
+++ Src/NetLts/WpfUserControlLib/Anime/ArcPi.xaml.cs
@@ -8,5 +8,28 @@ public partial class ArcPi
   public static readonly DependencyProperty RadiuProperty = DependencyProperty.Register("Radiu", typeof(double), typeof(ArcPi), new PropertyMetadata(50d)); public double Radiu { get => (double)GetValue(RadiuProperty); set => SetValue(RadiuProperty, value); }
   public static readonly DependencyProperty AngleProperty = DependencyProperty.Register("Angle", typeof(double), typeof(ArcPi), new PropertyMetadata(0d, PropertyChangedCallback)); public double Angle { get => (double)GetValue(AngleProperty); set => SetValue(AngleProperty, value); }
   static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((ArcPi)d).DrawArc((double)e.NewValue);
-  void DrawArc(double anglePercent) => ArcHelper.DrawArc(arc_path, new Point(Radiu, Radiu), Radiu - (arc_path.StrokeThickness / 2), 0, anglePercent * Math.PI * .02);
+  void DrawArc(double anglePercent) => DrawArc(arc_path, new Point(Radiu, Radiu), Radiu - (arc_path.StrokeThickness / 2), 0, anglePercent * Math.PI * .02);
+
+  static void DrawArc(System.Windows.Shapes.Path path, Point center, double radius, double angleBgn_, double angleEnd_)
+  {
+    const double _2Pi = Math.PI * 2;
+    var angleBgn = ((angleEnd_ < angleBgn_ ? angleEnd_ : angleBgn_ % _2Pi) + _2Pi) % _2Pi;
+    var angleEnd = ((angleEnd_ < angleBgn_ ? angleBgn_ : angleEnd_ % _2Pi) + _2Pi) % _2Pi; //if (angleEnd < angleBgn)      (angleBgn, angleEnd) = (angleEnd, angleBgn); //tu: swap!!!
+
+    var pathFigure = new PathFigure
+    {
+      StartPoint = new Point(center.X + (radius * Math.Cos(angleBgn)), center.Y + (radius * Math.Sin(angleBgn))) // :set start point of arc.
+    };
+
+    pathFigure.Segments.Add(new ArcSegment
+    {
+      Size = new Size(radius, radius),
+      Point = new Point(center.X + (radius * Math.Cos(angleEnd)), center.Y + (radius * Math.Sin(angleEnd))), // :set end point of arc.
+      IsLargeArc = angleEnd - angleBgn >= Math.PI,
+      SweepDirection = SweepDirection.Clockwise
+    });
+
+    path.Data = new PathGeometry();
+    ((PathGeometry)path.Data).Figures.Add(pathFigure);
+  }
 }
