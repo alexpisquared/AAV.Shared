5a41028998d70e4daf86543919e7e253eae3e48e
alexpisqueared <alex.pigida@outlook.com>
2025-03-23 17:24:35 -0400

    [main] Refactor sleep logic and enhance service registration
    * Reorganized conditional logic in `ScrSvrPresets.cs` for minimum sleep time based on machine name, adding new conditions and removing commented lines.
    * Renamed and updated `NewMethod` to `SwitchByPcName` in `MvvmInitHelper.cs`, improving service registration handling and adding a new case for "NewDev."
    * Introduced `FromOutlookCrashChecker` method in `App.xaml.cs` to monitor Outlook crashes, with asynchronous checks and logging enhancements in the `OnExit` method.
    * Updated `launchSettings.json` to remove unnecessary command line arguments for the "OleksaScrSvr" profile.
    
    by Oleksa at HO

--- Src/NetLts/StandardLib/Consts/ScrSvrPresets.cs
+++ Src/NetLts/StandardLib/Consts/ScrSvrPresets.cs
@@ -3,9 +3,10 @@ public class ScrSvrPresets
 {
   public static double MinToPcSleep =>
     DevOps.IsDbg ? 55 : //0.25 :  // for testing, etc....     
-                           //= "BEELINK1" ? 16 : // logs show 10 min limit kicks in here: "inf ╘══10.0 min OnExit"  ==> 8 min must be used
-    Environment.MachineName == "RAZER1" ? 56 :   // 1hr for: PerfectMind registration, etc....
-    Environment.MachineName == "ASUS2" ? 16 :    // see the logs if 10 min limit kicks in here
-    Environment.MachineName == "YOGA1" ? 16 :    // see the logs if 10 min limit kicks in here
+                        //= "BEELINK1" ? 16 :           // logs show 10 min limit kicks in here: "inf ╘══10.0 min OnExit"  ==> 8 min must be used
+    Environment.MachineName == "RAZER1" ? 56 :          // 1hr for: PerfectMind registration, etc....
+    Environment.MachineName == "ASUS2" ? 16 :           // see the logs if 10 min limit kicks in here
+    Environment.MachineName == "YOGA1" ? 16 :           // see the logs if 10 min limit kicks in here
+    Environment.MachineName.Contains("33") ? 24 * 60 :  // Mar 2025: not sure I need to sleep it here
     8.5; // something closes the app exactly on 10 min mark: "inf ╘══10.0 min OnExit"  ==> keep it under 10 > 9.5=8.5+1+.5  
 }
