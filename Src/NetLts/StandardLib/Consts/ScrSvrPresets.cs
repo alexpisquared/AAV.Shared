ccd04d6627f90b657c1e0b9f841e80e1cc156745
alexpisqueared <alex.pigida@outlook.com>
2025-04-04 12:16:57 -0400

    [main] pau

--- Src/NetLts/StandardLib/Consts/ScrSvrPresets.cs
+++ Src/NetLts/StandardLib/Consts/ScrSvrPresets.cs
@@ -3,10 +3,10 @@ public class ScrSvrPresets
 {
   public static double MinToPcSleep =>
     DevOps.IsDbg ? 55 : //0.25 :  // for testing, etc....     
-                        //= "BEELINK1" ? 16 :           // logs show 10 min limit kicks in here: "inf ╘══10.0 min OnExit"  ==> 8 min must be used
-    Environment.MachineName == "RAZER1" ? 56 :          // 1hr for: PerfectMind registration, etc....
-    Environment.MachineName == "ASUS2" ? 16 :           // see the logs if 10 min limit kicks in here
-    Environment.MachineName == "YOGA1" ? 16 :           // see the logs if 10 min limit kicks in here
-    Environment.MachineName.Contains("33") ? 56 :       // Mar 2025: not sure I need to sleep it here. Apr: better turn it off .. along with the monitor.
+                        //= "BEELINK1" ? 16 :   // logs show 10 min limit kicks in here: "inf ╘══10.0 min OnExit"  ==> 8 min must be used
+    Environment.MachineName == "RAZER1" ? 56 :  // 1hr for: PerfectMind registration, etc....
+    Environment.MachineName == "ASUS2" ? 16 :   // see the logs if 10 min limit kicks in here
+    Environment.MachineName == "YOGA1" ? 16 :   // see the logs if 10 min limit kicks in here
+    Environment.MachineName.Contains("33") ? 56 + 60 + 60 :  // Mar 2025: not sure I need to sleep it here. Apr 3: better turn it off .. along with the monitor. Apr 4: ScrSvr puts it to some sort of deep sleep which takes forever to wake up from: let's keep it off for 3 hours at least.
     8.5; // something closes the app exactly on 10 min mark: "inf ╘══10.0 min OnExit"  ==> keep it under 10 > 9.5=8.5+1+.5  
 }
