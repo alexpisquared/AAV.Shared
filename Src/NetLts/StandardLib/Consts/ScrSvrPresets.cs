529547559aed9a116dd3891402a683762699d085
AlexPi <alex.pigida@outlook.com>
2025-06-03 22:06:56 -0400

    Update package versions and refine sleep logic
    Updated the `HtmlAgilityPack` from `1.11.74` to `1.12.1` and `System.Drawing.Common` from `9.0.2` to `9.0.5` in `WebScrap.csproj` to enhance functionality and security.
    
    Modified `ScrSvrPresets.cs` to adjust sleep duration logic based on machine names, adding a condition for `NUC2` and reordering conditions for `ASUS2` and `RAZER1`.
    
    Updated package references in multiple project files:
    - `EventLogLib.csproj`: `System.Diagnostics.EventLog` from `9.0.2` to `9.0.5`
    - `OutlookCrashMonitor.csproj`: `Microsoft.Extensions.Configuration.UserSecrets` from `6.0.1` to `9.0.5`
    - `Radar.csproj`: `Microsoft.Extensions.Configuration.UserSecrets` from `9.0.4` to `9.0.5`
    
    Additionally, changed `ShowInTaskbar` property in `LongStretchAlertPopup.xaml` from `False` to `True` to modify window behavior in the taskbar.

--- Src/NetLts/StandardLib/Consts/ScrSvrPresets.cs
+++ Src/NetLts/StandardLib/Consts/ScrSvrPresets.cs
@@ -4,9 +4,10 @@ public class ScrSvrPresets
   public static double MinToPcSleep =>
     DevOps.IsDbg ? 55 : //0.25 :  // for testing, etc....     
                         //= "BEELINK1" ? 16 :   // logs show 10 min limit kicks in here: "inf ╘══10.0 min OnExit"  ==> 8 min must be used
-    Environment.MachineName == "RAZER1" ? 56 :  // 1hr for: PerfectMind registration, etc....
-    Environment.MachineName == "ASUS2" ? 16 :   // see the logs if 10 min limit kicks in here
+    Environment.MachineName == "NUC2" ? 26 :    // 
     Environment.MachineName == "YOGA1" ? 16 :   // see the logs if 10 min limit kicks in here
+    Environment.MachineName == "ASUS2" ? 16 :   // see the logs if 10 min limit kicks in here
+    Environment.MachineName == "RAZER1" ? 56 :  // 1hr for: PerfectMind registration, etc....
     Environment.MachineName.Contains("33") ? 14 :  // Mar 2025: not sure I need to sleep it here. Apr 3: better turn it off .. along with the monitor. Apr 4: ScrSvr puts it to some sort of deep sleep which takes forever to wake up from: let's keep it off for 3 hours at least.
     56 + 60 + 60 + 60 + 60 + 60; // MF1 and *: 5 hours is the max available from the system.
 }
