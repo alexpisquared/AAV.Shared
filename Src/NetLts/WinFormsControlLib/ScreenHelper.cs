1591b72f1314a0bb6c5a4f0eb0406318002365e6
AlexPi <alex.pigida@outlook.com>
2025-10-05 10:34:49 -0400

    Refactor GetSumOfAllBounds to handle multiple screens
    Refactored GetSumOfAllBounds to calculate the union of all screen bounds dynamically.

--- Src/NetLts/WinFormsControlLib/ScreenHelper.cs
+++ Src/NetLts/WinFormsControlLib/ScreenHelper.cs
@@ -7,7 +7,19 @@ public class ScreenHelper
   public static Screen PrimaryScreen => Screen.PrimaryScreen ?? throw new ArgumentNullException("▄▀▄▀▄ No Primary Screen ▀▄▀▄▀");
   public static Screen SecondaryScreen => (Screen.AllScreens.Length == 1 ? Screen.PrimaryScreen : Screen.AllScreens.First(r => r != Screen.PrimaryScreen)) ?? throw new ArgumentNullException("▄▀▄▀▄ No Primary nor Secondary Screen ▀▄▀▄▀");
 
-  public static Rectangle GetSumOfAllBounds => Rectangle.Union(PrimaryScreen.Bounds, SecondaryScreen.Bounds);
+   public static Rectangle GetSumOfAllBounds
+  {
+    get
+    {
+      var screens = GetAllScreens;
+      if (screens.Length == 0) return Rectangle.Empty;
+      var union = screens[0].Bounds;
+      for (int i = 1; i < screens.Length; i++) union = Rectangle.Union(union, screens[i].Bounds);
+
+      return union;
+    }
+  }
+
   public static Rectangle GetFirstNonPrimaryScreenBounds()
   {
     var f = Screen.AllScreens.FirstOrDefault(s => !s.Primary)?.Bounds ?? new Rectangle(0, 0, 1920, 1080);
