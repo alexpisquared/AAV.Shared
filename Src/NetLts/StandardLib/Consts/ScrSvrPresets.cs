f77bef7a89f522b2ba2453e8668d90b6c8a5097c
AlexPi <alex.pigida@outlook.com>
2025-12-08 13:06:08 -0500

    pau {}

--- Src/NetLts/StandardLib/Consts/ScrSvrPresets.cs
+++ Src/NetLts/StandardLib/Consts/ScrSvrPresets.cs
@@ -9,6 +9,6 @@ public class ScrSvrPresets
     Environment.MachineName == "ASUS2" ? 16 :   // see the logs if 10 min limit kicks in here
     Environment.MachineName == "RAZER1" ? 56 :  // 1hr for: PerfectMind registration, etc....
     Environment.MachineName == "MINISFORUM1" ? 56 + 60 + 60 + 60 + 60 + 60 + 60 + 60 :  // 8 HR!?!?!
-    Environment.MachineName.Contains("33") || Environment.MachineName.Contains("P22") ? 14 :  // Mar 2025: not sure I need to sleep it here. Apr 3: better turn it off .. along with the monitor. Apr 4: ScrSvr puts it to some sort of deep sleep which takes forever to wake up from: let's keep it off for 3 hours at least.
+    ((Environment.MachineName.Contains("33") || Environment.MachineName.Contains("P22"))) ? 14 :  // Mar 2025: not sure I need to sleep it here. Apr 3: better turn it off .. along with the monitor. Apr 4: ScrSvr puts it to some sort of deep sleep which takes forever to wake up from: let's keep it off for 3 hours at least.
     56 + 60 + 60 + 60 + 60 + 60; // Note: 5 hours is the max available from the system.
 }
