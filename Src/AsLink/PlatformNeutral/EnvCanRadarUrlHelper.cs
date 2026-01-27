fe7c7e48254acd4af717ce75e15c8932b41a04a8
AlexPi <alex.pigida@outlook.com>
2026-01-26 19:54:48 -0500

    Update projects to .NET 10.0 and minor code cleanup
    Upgraded all .csproj and .pubxml files from .NET 9.0 to .NET 10.0 for consistency and compatibility with the latest framework. Simplified the namespace and improved the IsDark logic in EnvCanRadarUrlHelper.cs. Removed an unused file reference in Radar.csproj. No other functional changes were made.

--- Src/AsLink/PlatformNeutral/EnvCanRadarUrlHelper.cs
+++ Src/AsLink/PlatformNeutral/EnvCanRadarUrlHelper.cs
@@ -1,34 +1,31 @@
-﻿using System;
+﻿namespace AsLink;
 
-namespace AsLink
+public class EnvCanRadarUrlHelper //2021: this is the single central intel used/shared by all radar consumers, including: ToRunOr, AlexPi.Scr, etc. True on 2022-02-04.
 {
-  public class EnvCanRadarUrlHelper //2021: this is the single central intel used/shared by all radar consumers, including: ToRunOr, AlexPi.Scr, etc. True on 2022-02-04.
+  public static string GetRadarUrl(DateTime d) => $"https://dd.meteo.gc.ca/today/radar/DPQPE/GIF/CASKR/{d:yyyyMMddTHHmm}Z_MSC_Radar-DPQPE_CASKR_{((DateTime.Today.Month < 3) ? "Snow" : "Rain")}.gif";   // https://dd.meteo.gc.ca/today/radar/DPQPE/GIF/CASKR/    every 6 min     cleaned dark theme only
+  public static string GetRadarUrl_Messy(DateTime d) => $"https://dd.meteo.gc.ca/today/radar/PRECIPET/GIF/WKR/{d:yyyyMMddHHmm}_WKR_COMP_PRECIPET_RAIN.gif";  // https://dd.meteo.gc.ca/today/radar/PRECIPET/GIF/WKR/   every 10 min    messy images    
+  public static string GetRadarUrl_NotRadarFallback(DateTime d) => $"https://weather.gc.ca/data/satellite/goes_ecan_1070_m_{d.Year}@{d.Month:0#}@{d.Day:0#}_{d.Hour:0#}h{d.Minute:0#}m.jpg";  // <===========================================
+  public static string GetRadarUrl_OLD(DateTime d, string rsRainOrSnow /*= "RAIN"*/, string station /*= "WKR"*/, bool isFallbackCAPPI, bool isFallbackCOMP = false)
   {
-    public static string GetRadarUrl(DateTime d) => $"https://dd.meteo.gc.ca/today/radar/DPQPE/GIF/CASKR/{d:yyyyMMddTHHmm}Z_MSC_Radar-DPQPE_CASKR_{((DateTime.Today.Month < 3) ? "Snow" : "Rain")}.gif";   // https://dd.meteo.gc.ca/today/radar/DPQPE/GIF/CASKR/    every 6 min     cleaned dark theme only
-    public static string GetRadarUrl_Messy(DateTime d) => $"https://dd.meteo.gc.ca/today/radar/PRECIPET/GIF/WKR/{d:yyyyMMddHHmm}_WKR_COMP_PRECIPET_RAIN.gif";  // https://dd.meteo.gc.ca/today/radar/PRECIPET/GIF/WKR/   every 10 min    messy images    
-    public static string GetRadarUrl_NotRadarFallback(DateTime d) => $"https://weather.gc.ca/data/satellite/goes_ecan_1070_m_{d.Year}@{d.Month:0#}@{d.Day:0#}_{d.Hour:0#}h{d.Minute:0#}m.jpg";  // <===========================================
-    public static string GetRadarUrl_OLD(DateTime d, string rsRainOrSnow /*= "RAIN"*/, string station /*= "WKR"*/, bool isFallbackCAPPI, bool isFallbackCOMP = false)
-    {
-      var cmp = isFallbackCOMP ? "_COMP" : "";
-      var cap = isFallbackCAPPI ? "CAPPI" : "PRECIP";
-      var drk = IsDark ? "" : "detailed/";
+    var cmp = isFallbackCOMP ? "_COMP" : "";
+    var cap = isFallbackCAPPI ? "CAPPI" : "PRECIP";
+    var drk = IsDark ? "" : "detailed/";
 
-      return $"https://weather.gc.ca/data/radar/{drk}temp_image//{station}/{station}{cmp}_{cap}_{rsRainOrSnow}_{d.Year}_{d.Month:0#}_{d.Day:0#}_{d.Hour:0#}_{d.Minute:0#}.GIF";
-    }
-
-    public static DateTime RoundBy10min(DateTime dt) => dt.AddTicks(-dt.Ticks % (10 * TimeSpan.TicksPerMinute));
-    public static bool IsDark => DateTime.Now.Hour < 8 || 20 <= DateTime.Now.Hour;
+    return $"https://weather.gc.ca/data/radar/{drk}temp_image//{station}/{station}{cmp}_{cap}_{rsRainOrSnow}_{d.Year}_{d.Month:0#}_{d.Day:0#}_{d.Hour:0#}_{d.Minute:0#}.GIF";
   }
+
+  public static DateTime RoundBy10min(DateTime dt) => dt.AddTicks(-dt.Ticks % (10 * TimeSpan.TicksPerMinute));
+  public static bool IsDark => DateTime.Now.Hour is < 8 or >= 20;
 }
 /*
 2022-01 Apparently, the old style GIFs are still available, but at the new location/format:
- https://dd.meteo.gc.ca/today/radar/PRECIPET/GIF/WKR/   every 10 min    messy images    
- https://dd.meteo.gc.ca/today/radar/PRECIPET/GIF/CASKR/ every 6/10 min  messy images   
- https://dd.meteo.gc.ca/today/radar/DPQPE/GIF/CASKR/    every 6 min     cleaned dark theme only
+https://dd.meteo.gc.ca/today/radar/PRECIPET/GIF/WKR/   every 10 min    messy images    
+https://dd.meteo.gc.ca/today/radar/PRECIPET/GIF/CASKR/ every 6/10 min  messy images   
+https://dd.meteo.gc.ca/today/radar/DPQPE/GIF/CASKR/    every 6 min     cleaned dark theme only
 
 2021-11-21  Docs/Info:
- https://eccc-msc.github.io/open-data/msc-data/obs_radar/readme_radar_en/
- https://eccc-msc.github.io/open-data/msc-data/obs_radar/readme_radarimage-datamart_en/
+https://eccc-msc.github.io/open-data/msc-data/obs_radar/readme_radar_en/
+https://eccc-msc.github.io/open-data/msc-data/obs_radar/readme_radarimage-datamart_en/
 
 2021-04-05  Latest cloud cover image: ir, vis, ir+vis:
 https://weather.gc.ca/data/satellite/goes_ecan_visible_100.jpg
